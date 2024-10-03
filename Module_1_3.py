b7bc871cdadd3b569e79e13fbe736ffaf58b70f7
IgorPyrsikov <IgorPyrsikov@GMail.com>
2024-10-04 01:15:27 +0500

    Add files via upload

--- Module_1_3.py
+++ Module_1_3.py
@@ -6,3 +6,9 @@ new_age = ('New age: 36')
 print(new_age)
 is_student = 'Is Student: True'
 print(is_student)
+
+number_of_completed_ha = 12
+number_of_hours_spent = 1.5
+course_name = 'Курс: Python,'
+time_fo_one_real = number_of_hours_spent / number_of_completed_ha
+print(course_name, 'всего задач:', number_of_completed_ha, ', затрачено часов:', number_of_hours_spent, ', среднее время выполнения ', time_fo_one_real, 'часа.')
