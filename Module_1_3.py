974574e4ade905b4737e7cd4c41fc945b58a41b5
IgorPyrsikov <IgorPyrsikov@GMail.com>
2024-10-04 13:44:28 +0500

    Add files via upload

--- Module_1_3.py
+++ Module_1_3.py
@@ -1,14 +1,24 @@
+print('Динамическая типизация')
 name = 'Name: Igor'
 print(name)
 age = 'Age: 35'
 print(age)
-new_age = ('New age: 36')
+new_age = 'New age: 36'
 print(new_age)
 is_student = 'Is Student: True'
 print(is_student)
 
+print('Переменные')
 number_of_completed_ha = 12
 number_of_hours_spent = 1.5
 course_name = 'Курс: Python,'
 time_fo_one_real = number_of_hours_spent / number_of_completed_ha
 print(course_name, 'всего задач:', number_of_completed_ha, ', затрачено часов:', number_of_hours_spent, ', среднее время выполнения ', time_fo_one_real, 'часа.')
+
+print('Строки и индексация строк')
+example = 'Пример'
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1:6:2])
